d30b2823c6bf7127928b4e7682b7126e807169a9
Evgeniya Kireeva <evil-nekromat777@mail.ru>
2020-10-09 16:36:12 +0300

    feat: функция перемешивания

--- script.js
+++ script.js
@@ -2,9 +2,9 @@
 const boxElem = document.querySelector('.box');
 const resetButtonElem = document.querySelector('.button_type_reset');
 const startButtonElem = document.querySelector('.button_type_start');
-console.log(startButtonElem);
 
 
+// Инициализация ячеек
 let cells = [];
 for (let i = 1; i < 16; i += 1) {
     cells.push(i);
@@ -13,9 +13,22 @@ cells.push(0)
 
 renderCells();
 
-/**
- * Рендерит ячейки
- */
+
+// Вешаем обработчики на кнопки
+resetButtonElem.addEventListener('click', function () {
+    resetCells();
+
+})
+
+// Функция перемешивает ячейки
+function resetCells() {
+    cells = _.shuffle(cells);
+    const cellElems = document.querySelectorAll('.cell')
+    cells.forEach((n, i) => updateCell(cellElems[i], n));
+}
+
+
+// Функция рендерит ячейки
 function renderCells() {
     const cellTemplate = document.querySelector('[data-component="cell"]');
 
@@ -32,3 +45,4 @@ function updateCell(cellElem, number) {
     cellNumElem.textContent = number
     cellElem.id = number
 }
+
